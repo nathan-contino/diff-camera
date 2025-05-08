d18b72b35072fbf6dc1989b407e525fa12191d9c
nathan contino <nathan.contino@viam.com>
2025-05-08 15:51:47 -0400

    add reload.sh to module tar

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pip3 install -r requirements.txt
 python3 -m PyInstaller --onefile src/main.py
-tar -czf module.tar.gz run.sh requirements.txt src meta.json build.sh setup.sh
+tar -czf module.tar.gz run.sh requirements.txt src meta.json build.sh setup.sh reload.sh
