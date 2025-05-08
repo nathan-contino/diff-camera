b47f17049130e36b10d09e7ec36e5c1e12a51a0f
nathan contino <nathan.contino@viam.com>
2025-05-08 15:46:54 -0400

    for python modules ONLY

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pip3 install -r requirements.txt
 python3 -m PyInstaller --onefile src/main.py
-tar -czvf dist/archive.tar.gz dist/main
+tar -czf module.tar.gz run.sh requirements.txt src meta.json build.sh setup.sh
