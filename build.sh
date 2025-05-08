49b18500189451f9599b2622d07ca1ad6185f091
nathan contino <nathan.contino@viam.com>
2025-05-08 12:24:52 -0400

    Fix build

--- build.sh
+++ build.sh
@@ -9,5 +9,13 @@ if ! $PYTHON -m pip install pyinstaller -Uqq; then
     exit 1
 fi
 
-$PYTHON -m PyInstaller --onefile --hidden-import="googleapiclient" src/main.py
-tar -czvf dist/archive.tar.gz ./dist/main
+# Build for linux-arm64
+$PYTHON -m PyInstaller --onefile \
+    --hidden-import="googleapiclient" \
+    --target-arch=arm64 \
+    --target-platform=linux \
+    src/main.py
+
+# Create the module archive
+rm -f module.tar.gz
+tar -czvf module.tar.gz requirements.txt src/*.py src/models/*.py meta.json setup.sh reload.sh dist/main
