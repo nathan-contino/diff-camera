e9bd582b7dd60c23ac06754ed6491d7a80e1b67d
nathan contino <nathan.contino@viam.com>
2025-05-08 15:14:05 -0400

    Fix the build

--- build.sh
+++ build.sh
@@ -1,19 +1,4 @@
-#!/bin/sh
-cd `dirname $0`
-
-# Create a virtual environment to run our code
-VENV_NAME="venv"
-PYTHON="$VENV_NAME/bin/python"
-
-if ! $PYTHON -m pip install pyinstaller -Uqq; then
-    exit 1
-fi
-
-# Build the executable
-$PYTHON -m PyInstaller --onefile \
-    --hidden-import="googleapiclient" \
-    src/main.py
-
-# Create the module archive
-rm -f dist/module.tar.gz
-tar -czvf dist/module.tar.gz dist/main
+#!/bin/bash
+pip3 install -r requirements.txt
+python3 -m PyInstaller --onefile src/main.py
+tar -czvf dist/archive.tar.gz dist/main
